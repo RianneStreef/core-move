652b0dc2286b1abaad0794c07684c8e79578639e
RianneStreef <riannestreef@gmail.com>
2021-09-29 16:08:29 +0200

    legal

--- src/pages/legal.js
+++ src/pages/legal.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-const legal = () => {
-  return <div>Wettelijke vermeldingen</div>;
+import Layout from "../components/Layout.jsx";
+import Legal from "../components/Legal.jsx";
+
+const LegalPage = () => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Core Move </title>
+      </Helmet>
+      <Layout>
+        <div className="content">
+          <Legal />
+        </div>
+      </Layout>
+    </div>
+  );
 };
 
-export default legal;
+export default LegalPage;
